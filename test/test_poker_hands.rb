9aceb86a2b8e0f141985e995ea8da4bcd692127c
Rico Jones <rico@toasterlovin.com>
2017-06-25 00:06:34 +0000

    Add spacing to make tests more readable

--- test/test_poker_hands.rb
+++ test/test_poker_hands.rb
@@ -5,8 +5,10 @@ class PokerTest < Minitest::Test
   def test_dentifies_straight_flush
     flush = Poker::HandFactory.hand('2H 3H 4H 5H 7H')
     refute_instance_of Poker::StraightFlush, flush
+
     straight = Poker::HandFactory.hand('2H 3H 4H 5H 6S')
     refute_instance_of Poker::StraightFlush, straight
+
     straight_flush = Poker::HandFactory.hand('2H 3H 4H 5H 6H')
     assert_instance_of Poker::StraightFlush, straight_flush
   end
@@ -14,6 +16,7 @@ class PokerTest < Minitest::Test
   def test_identifies_four_of_a_kind
     not_four_of_a_kind = Poker::HandFactory.hand('2S 2H 2D 3C 4S')
     refute_instance_of Poker::FourOfAKind, not_four_of_a_kind
+
     four_of_a_kind = Poker::HandFactory.hand('2S 2H 2D 2C 3S')
     assert_instance_of Poker::FourOfAKind, four_of_a_kind
   end
@@ -21,6 +24,7 @@ class PokerTest < Minitest::Test
   def test_identifies_full_house
     not_full_house = Poker::HandFactory.hand('2S 2H 3D 3C 4S')
     refute_instance_of Poker::FullHouse, not_full_house
+
     full_house = Poker::HandFactory.hand('2S 2H 2D 3C 3S')
     assert_instance_of Poker::FullHouse, full_house
   end
@@ -28,6 +32,7 @@ class PokerTest < Minitest::Test
   def test_identifies_flush
     not_flush = Poker::HandFactory.hand('2H 3H 4H 5H 6C')
     refute_instance_of Poker::Flush, not_flush
+
     flush = Poker::HandFactory.hand('2H 3H 4H 5H 7H')
     assert_instance_of Poker::Flush, flush
   end
@@ -35,6 +40,7 @@ class PokerTest < Minitest::Test
   def test_identifies_straight
     not_straight = Poker::HandFactory.hand('2S 3H 4D 5C 5S')
     refute_instance_of Poker::Straight, not_straight
+
     straight = Poker::HandFactory.hand('2S 3H 4D 5C 6S')
     assert_instance_of Poker::Straight, straight
   end
@@ -42,6 +48,7 @@ class PokerTest < Minitest::Test
   def test_identifies_three_of_a_kind
     not_three_of_a_kind = Poker::HandFactory.hand('2S 2H 2D 3C 3S')
     refute_instance_of Poker::ThreeOfAKind, not_three_of_a_kind
+
     three_of_a_kind = Poker::HandFactory.hand('2S 2H 2D 3C 4S')
     assert_instance_of Poker::ThreeOfAKind, three_of_a_kind
   end
@@ -49,6 +56,7 @@ class PokerTest < Minitest::Test
   def test_identifies_two_pair
     not_two_pair = Poker::HandFactory.hand('2S 2H 3D 4C 5S')
     refute_instance_of Poker::TwoPair, not_two_pair
+
     two_pair = Poker::HandFactory.hand('2S 2H 3D 3C 4S')
     assert_instance_of Poker::TwoPair, two_pair
   end
@@ -56,6 +64,7 @@ class PokerTest < Minitest::Test
   def test_identifies_one_pair
     not_one_pair = Poker::HandFactory.hand('2S 3H 4D 5C 7S')
     refute_instance_of Poker::OnePair, not_one_pair
+
     one_pair = Poker::HandFactory.hand('2S 2H 3D 4C 5S')
     assert_instance_of Poker::OnePair, one_pair
   end
