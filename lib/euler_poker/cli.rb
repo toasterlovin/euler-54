f9d22bc6777d23be2b80dcec8ac370fcc7427308
Rico Jones <rico@toasterlovin.com>
2017-06-26 00:06:58 +0000

    Improve organization of EulerPoker::CLI

--- lib/euler_poker/cli.rb
+++ lib/euler_poker/cli.rb
@@ -3,17 +3,25 @@ module EulerPoker
     attr_reader :results
 
     def initialize(file)
-      @results = []
+      @file = File.open(file, 'r')
+      print_results
+    end
 
-      File.open(file, 'r') do |f|
-        f.each_line do |line|
-          round = EulerPoker::Round.new(line)
-          @results << round.winner
-        end
+    private
+
+    def print_results
+      hands = 0
+      results = []
+
+      @file.each_line do |line|
+        round = EulerPoker::Round.new(line)
+
+        results << round.winner
+        hands += 1
       end
+      @file.close
 
-      puts "First hand won #{@results.count(:first)} times"
-      puts "Second hand won #{@results.count(:second)} times"
+      puts "First hand won #{results.count(:first)} out of #{hands} hands."
     end
   end
 end
--- test/test_cli.rb
+++ test/test_cli.rb
@@ -4,8 +4,7 @@ require 'pty'
 class CLITest < Minitest::Test
   def test_file_evaluation
     PTY.spawn('bin/euler_poker test/files/poker.txt') do |output, input, pid|
-      assert_equal 'First hand won 1 times', output.gets.chomp
-      assert_equal 'Second hand won 1 times', output.gets.chomp
+      assert_equal 'First hand won 1 out of 2 hands.', output.gets.chomp
     end
   end
 end
